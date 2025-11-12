aa0b67990ad4d23594163d5dad42542f98661f50
Raquel Vales <raquelaweb@gmail.com>
2025-11-12 16:55:25 +0000

    Vista completa con menu

--- src/main/java/vista/VistaTaller.java
+++ src/main/java/vista/VistaTaller.java
@@ -1,5 +1,46 @@
 package vista;
 
-public class VistaTaller {
+import java.util.Scanner;
 
+public class VistaTaller {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int opcion;
+		do {
+			menuNavegacionGeneral();
+			opcion = sc.nextInt();
+			sc.nextLine(); // LIMPIAR BUFFER
+
+			switch (opcion) {
+			case 1:
+				iniciarSesion(sc);
+				break;
+			case 2:
+				verReparacionesFinalizadas();
+				break;
+			default:
+				System.out.println("Opción no válida. Inténtalo de nuevo.");
+			}
+
+		} while (opcion != 0);
+		System.out.println("Saliendo del sistema...");
+		sc.close();
+
+	}
+
+	public static void iniciarSesion(Scanner sc) {
+
+	}
+
+	public static void verReparacionesFinalizadas() {
+
+	}
+
+	public static void menuNavegacionGeneral() {
+		System.out.println("\\MENÚ DE LA APLICACIÓN//");
+		System.out.println("Escoga que quiere hacer: \n");
+		System.out.println("0. Salir de la aplicación");
+		System.out.println("1. Iniciar sesión");
+		System.out.println("2. Ver reparaciones finalizadas");
+	}
 }
