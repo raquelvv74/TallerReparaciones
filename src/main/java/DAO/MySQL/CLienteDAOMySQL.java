fee99111fc2a4ae716eb8db3c410e4a17e295bae
Raquel Vales <raquelaweb@gmail.com>
2025-11-18 19:20:12 +0000

    Cambiado el nombre que tenia una mayuscula mal puesta

--- src/main/java/DAO/MySQL/CLienteDAOMySQL.java
+++
@@ -1,9 +0,0 @@
-package DAO.MySQL;
-
-public class CLienteDAOMySQL {
-	int insert(Cliente c);
-	int update(Cliente c);
-	int delete(String dni);
-	ArrayList<Cliente c> findall();
-	Cliente findByDni(String dni);
-}
---
+++ src/main/java/DAO/MySQL/ClienteDAOMySQL.java
@@ -0,0 +1,21 @@
+package DAO.MySQL;
+
+import entities.Cliente;
+
+public class ClienteDAOMySQL {
+	int insert(Cliente c) {
+		
+	}
+	int update(Cliente c) {
+		
+	}
+	int delete(String dni){
+		
+	}
+	ArrayList<Cliente c> findall(){
+		
+	}
+	Cliente findByDni(String dni){
+		
+	}
+}
